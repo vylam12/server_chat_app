08e7a8cf21bf7506a3d76e9fb51ed5e8d833ad8e
vylam12 <lethaovytran7@gmail.com>
2025-04-24 11:56:31 +0700

    update 9

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
+import Vocabulary from "../models/vocabulary.js";
+import fs from "fs";
 
+
+const vocabList = JSON.parse(fs.readFileSync("vocab.json", "utf8"));
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log(" Connect database success");
+
+        const bulkOps = vocabList.map(vocab => ({
+            updateOne: {
+                filter: { word: vocab.word },
+                update: { $set: vocab },
+                upsert: true
+            }
+        }));
+
+        if (bulkOps.length > 0) {
+            await Vocabulary.bulkWrite(bulkOps);
+            console.log("Đã xử lý danh sách từ vựng!");
+        }
+
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
     }
